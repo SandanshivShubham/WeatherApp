20950a1bc241915ae96e7936c34582847bfc4e53
Sandanshivshubham <sandanshiv16@gmail.com>
2022-07-30 12:20:40 +0530

    nodejs project

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "5e755d6338857b3e726d2e643fa21df4"
+    }
+}
+
+module.exports = constants;
